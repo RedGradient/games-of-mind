2b1b8c2463b987036bdea74bd78ff66270b894d4
redgradient <neutral1shade@yahoo.com>
2022-10-08 15:05:56 +0400

    changed the order of the list of games: was desc, now asc; input occurs in one line with "Your choice" text

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -13,17 +13,16 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Please enter the game number and press Enter.");
 
-        // game menu
-        System.out.println("5 - Progression");
-        System.out.println("4 - GCD");
-        System.out.println("3 - Calculator");
-        System.out.println("2 - Even");
-        System.out.println("1 - Greet");
         System.out.println("0 - Exit");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("3 - Calculator");
+        System.out.println("4 - GCD");
+        System.out.println("5 - Progression");
 
+        System.out.print("Your choice: ");
         Scanner sc = new Scanner(System.in);
         var choice = sc.nextInt();
-        System.out.println("Your choice: " + choice);
 
         System.out.println();
 
