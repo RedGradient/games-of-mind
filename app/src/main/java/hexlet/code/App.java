4a6540f223ec0a8b4b9b9f3db2b7dbc1a7ea8ee5
redgradient <neutral1shade@yahoo.com>
2022-10-09 16:17:01 +0400

    step 9

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,22 +5,16 @@ import hexlet.code.Games.*;
 import java.util.Scanner;
 
 public class App {
-    private static String askForName() {
-        System.out.print("May I have your name? ");
-        Scanner sc = new Scanner(System.in);
-        var name = sc.next();
-        System.out.println("Hello, " + name + "!");
-        return name;
-    }
     public static void main(String[] args) {
         System.out.println("Please enter the game number and press Enter.");
 
-        System.out.println("0 - Exit");
         System.out.println("1 - Greet");
         System.out.println("2 - Even");
         System.out.println("3 - Calculator");
         System.out.println("4 - GCD");
         System.out.println("5 - Progression");
+        System.out.println("6 - Prime");
+        System.out.println("0 - Exit");
 
         System.out.print("Your choice: ");
         Scanner sc = new Scanner(System.in);
@@ -33,18 +27,11 @@ public class App {
 
             switch (choice) {
                 case 1 -> Cli.greeting();
-                case 2 -> {
-                    Even.play();
-                }
-                case 3 -> {
-                    Calculator.play();
-                }
-                case 4 -> {
-                    GCD.play();
-                }
-                case 5 -> {
-                    Progression.play();
-                }
+                case 2 -> Even.play();
+                case 3 -> Calculator.play();
+                case 4 -> GCD.play();
+                case 5 -> Progression.play();
+                case 6 -> Prime.play();
             }
         }
     }
---
+++ app/src/main/java/hexlet/code/Games/Prime.java
@@ -0,0 +1,47 @@
+package hexlet.code.Games;
+
+import hexlet.code.Engine;
+
+import java.util.HashMap;
+
+public class Prime {
+    private static final int RANDOM_NUMBER_RANGE = 100;
+
+    private static boolean isPrime(int inputNumber) {
+
+        if (inputNumber <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= inputNumber / 2; i++) {
+            if ((inputNumber % i) == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static HashMap<String, String> createQuestionAnswerPair() {
+        HashMap<String, String> questionAnswerPair = new HashMap<>();
+
+        var randomNumber = Engine.RAND.nextInt(RANDOM_NUMBER_RANGE);
+        var correctAnswer = (isPrime(randomNumber)) ? "yes" : "no";
+
+        questionAnswerPair.put(Integer.toString(randomNumber), correctAnswer);
+
+        return questionAnswerPair;
+    }
+
+    public static void play() {
+        var invitationQuestion = "Answer 'yes' if the number is prime, otherwise answer 'no'.";
+
+        HashMap<String, String> questionAnswerPairs = new HashMap<>();
+        for (var i = 0; i < Engine.MAX_ANSWERS_COUNT; i++) {
+            HashMap<String, String> pair = createQuestionAnswerPair();
+            questionAnswerPairs.putAll(pair);
+        }
+
+        Engine.run(invitationQuestion, questionAnswerPairs);
+    }
+}
