81d977c4d9d163de4b4756d7ac451dbc1c4d975c
redgradient <neutral1shade@yahoo.com>
2022-10-15 19:54:57 +0400

    renamed argument

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -7,7 +7,7 @@ public class Engine {
 
     public static final int MAX_ANSWERS_COUNT = 3;
 
-    public static void run(String description, HashMap<String, String> questionAnswerPairs) {
+    public static void run(String description, HashMap<String, String> roundsData) {
 
         System.out.print("May I have your name? ");
         var username = Utils.SC.next();
@@ -15,7 +15,7 @@ public class Engine {
 
         System.out.println(description);
 
-        for (Map.Entry<String, String> roundData : questionAnswerPairs.entrySet()) {
+        for (Map.Entry<String, String> roundData : roundsData.entrySet()) {
 
             var question = roundData.getKey();
             var correctAnswer = roundData.getValue();
